460688c1a6127f240886d4687b84bc3b32ddf336
lolstorm <ricardocampos65@hotmail.com>
2015-07-07 08:41:19 +0100

    improving some things on base stat and added get and seters

--- Fallout Rpg/Assets/Fallout Rpg/[Scripts]/Character Classes/BaseStat.cs
+++ Fallout Rpg/Assets/Fallout Rpg/[Scripts]/Character Classes/BaseStat.cs
@@ -2,24 +2,43 @@
 using System.Collections;
 
 public class BaseStat {
-	private int base_value;
-	private int exp_to_level;
-	private float level_modifier;
-	private int buff_value;
+	private int _base_value;
+	private int _exp_to_level;
+	private float _level_modifier;
+	private int _buff_value;
 
 	public BaseStat() {
-		base_value = 0;
-		buff_value = 0;
-		level_modifier = 1.0f;
-		exp_to_level = 100;
+		_base_value = 0;
+		_buff_value = 0;
+		_level_modifier = 1.0f;
+		_exp_to_level = 100;
 	}
 
 	public int calculate_exp_to_level() {
-		return (int)(exp_to_level * level_modifier);
+		return (int)(_exp_to_level * _level_modifier);
 	}
 
 	public void level_up() {
-		exp_to_level = calculate_exp_to_level ();
+		_exp_to_level = calculate_exp_to_level ();
 		base_value++;
 	}
+
+#region Setters and Getters
+	public int base_value{
+		get{return _base_value;}
+		set{ _base_value = value;}
+	}
+	public int exp_to_level{
+		get{return _exp_to_level;}
+		set{ _exp_to_level = value;}
+	}
+	public float level_modifier{
+		get{ return _level_modifier;}
+		set{ _level_modifier = value;}
+	}
+	public int buff_value{
+		get{return _buff_value;}
+		set{ _buff_value = value;}
+	}
+#endregion
 }
