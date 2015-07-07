1d181489e2c9b77c9e8cdf04e445c12d9cf4717c
lolstorm <ricardocampos65@hotmail.com>
2015-07-07 08:52:24 +0100

    added PrimaryStat class #2

---
+++ Fallout Rpg/Assets/Fallout Rpg/[Scripts]/Character Classes/PrimaryStat.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrimaryStat : BaseStat {
+	public PrimaryStat(){
+		exp_to_level = 50;
+		level_modifier = 1.0f;
+	}
+}
+public enum StatName {
+	Strength,
+	Agility,
+	Luck,
+	Charisma,
+	Perception,
+	Intelligence,
+	Endurance
+}
