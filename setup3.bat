12f612ae8e3f10116f1ce6a2477af33083195a37
lolstorm <ricardo.s.campos@ist.utl.pt>
2015-07-19 14:08:08 +0100

    added new things to setup files and a dependencie

--- setup3.bat
+++ setup3.bat
@@ -1,3 +1,3 @@
 @echo off
 virtualenv.exe -p C:\Python27\python.exe .venv
-start cmd /k ".\.venv\Scripts\activate.bat & pip install -r requirements.txt"
+start cmd /k "Dependencies\VCForPython27.msi & .\.venv\Scripts\activate.bat & pip install -r requirements.txt"
