2c2fa6a2fa7a91110f9070519902394a9389ca62
lolstorm <ricardocampos65@hotmail.com>
2015-07-19 09:56:35 +0100

    added fab file to make git commands easier but doesn't work on windows will work on it latter

---
+++ fabfile.py
@@ -0,0 +1,63 @@
+from fabric.api import local, env
+from fabric.operations import put, run
+from fabric.utils import abort
+import os
+#this shit doesn't work on windows for obvious reasons need to check this later
+
+# Do use this if you want to perform an action in virtualenv.
+def run_in_venv(cmd):
+    local('source venv/bin/activate && ' + cmd, shell='/bin/bash')
+
+def setup(aliased=False):
+    # set up virtualenv
+    if not ("venv" in os.listdir('.')):
+        local('sudo pip install virtualenv')
+        local('virtualenv venv')
+    run_in_venv('pip install -r requirements.txt')
+    print "Setup successfully done"
+
+def update_requirements(arg='s'):
+    run_in_venv('pip install -r requirements.txt')
+    install_sdk(arg)
+
+def freeze():
+    run_in_venv('pip freeze > requirements.txt')
+
+    # deleting private requirements
+    f = open('requirements.txt', 'r')
+    lines = f.readlines()
+    f.close()  # buffer all the lines
+
+    f = open('requirements.txt', 'w')
+    flag = False
+    for line in lines:  # rewrite all the lines except private requirements
+        for r in PRIVATE_REQUIREMENTS:
+            if r in line:
+                flag = True
+        if not flag:
+            f.write(line)
+            flag = False
+
+def pull_r():
+    local('git pull --rebase')
+
+
+def commit(msg):
+    # fab commit:"msg"
+    if msg[0] == '"':
+        msg = msg[1:]
+    if msg[-1] == '"':
+        msg = msg[:-1]
+
+    local('git add --all')
+    local('git commit -m \"' + msg + '\"')
+
+
+def push():
+    local('git push origin master')
+
+
+def git_all(msg):
+    commit(msg)
+    pull_r()
+    push()
