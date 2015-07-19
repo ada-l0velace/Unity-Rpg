a947bbd28ed94a6fd0388cf43c4cf301a3505d4d
lolstorm <ricardo.s.campos@ist.utl.pt>
2015-07-19 14:28:53 +0100

    solving some mistakes on scripts

--- setup2.bat
+++ setup2.bat
@@ -1,3 +1,4 @@
 @echo off
 powershell -Command "python get-pip.py" & pip install virtualenv & pip install virtualenvwrapper-powershell
+DEL /F /S /Q /A "get-pip.py"
 set /p temp="Now run Setup3"
--- setupfabric.bat
+++ setupfabric.bat
@@ -7,6 +7,5 @@ python-2.7.10.amd64.msi
 setx path "%path%;C:\Python27;C:\Python27\Scripts"
 powershell -Command "Invoke-WebRequest https://raw.github.com/pypa/pip/master/contrib/get-pip.py -OutFile get-pip.py"
 DEL /F /S /Q /A "python-2.7.10.amd64.msi"
-DEL /F /S /Q /A "get-pip.py"
 set /p temp="Everything is now installed now run setup2.bat"
 
