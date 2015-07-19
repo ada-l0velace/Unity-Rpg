672c45023cb00759f6cb3ab022b3bd12aca81658
lolstorm <ricardo.s.campos@ist.utl.pt>
2015-07-19 18:23:24 +0100

    Maybe now works on windows power shell 2.0

--- setupfabric.bat
+++ setupfabric.bat
@@ -2,10 +2,10 @@
 SETLOCAL
 SET var=%path%
 SET var2=%var:~-1%
-powershell -Command "Invoke-WebRequest https://www.python.org/ftp/python/2.7.10/python-2.7.10.amd64.msi -OutFile python-2.7.10.amd64.msi"
+powershell -Command "Start-BitsTransfer https://www.python.org/ftp/python/2.7.10/python-2.7.10.amd64.msi python-2.7.10.amd64.msi"
 python-2.7.10.amd64.msi
 setx path "%path%;C:\Python27;C:\Python27\Scripts"
-powershell -Command "Invoke-WebRequest https://raw.github.com/pypa/pip/master/contrib/get-pip.py -OutFile get-pip.py"
+powershell -Command "Start-BitsTransfer https://raw.github.com/pypa/pip/master/contrib/get-pip.py get-pip.py"
 DEL /F /S /Q /A "python-2.7.10.amd64.msi"
 set /p temp="Everything is now installed now run setup2.bat"
 
