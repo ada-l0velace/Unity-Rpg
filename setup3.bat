69cb4281e14e4dd34bd751d1c37e55f56fbcfcff
Ricardo João Silva <kewry.dev@gmail.com>
2015-07-20 04:29:40 +0100

    fabric test p2

--- setup3.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-virtualenv.exe -p C:\Python27\python.exe .venv
-powershell -Command "Start-BitsTransfer http://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi VCForPython27.msi"
-start cmd /k "VCForPython27.msi & DEL /F /S /Q /A VCForPython27.msi & .\.venv\Scripts\activate.bat & pip install -r requirements.txt"
--- setupfabric.bat
+++ setupfabric.bat
@@ -8,4 +8,3 @@ setx path "%path%;C:\Python27;C:\Python27\Scripts"
 powershell -Command "Start-BitsTransfer https://raw.github.com/pypa/pip/master/contrib/get-pip.py get-pip.py"
 DEL /F /S /Q /A "python-2.7.10.amd64.msi"
 set /p temp="Everything is now installed now run setup2.bat"
-
